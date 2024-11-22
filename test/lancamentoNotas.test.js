3b41b92a49eef74c5e603aaebe41d0fdf1d9b5c0
ArturRib <122729878+ArturRib@users.noreply.github.com>
2024-11-21 23:36:29 -0300

    Update lancamentoNotas.test.js

--- test/lancamentoNotas.test.js
+++ test/lancamentoNotas.test.js
@@ -1,25 +1,39 @@
-const { lancarNotas } = require("../scr/lancamentoNotas");
+const { lancarNota } = require("../scr/lancamentoNotas");
 
-test("Deve salvar notas válidas", () => {
-  expect(lancarNotas(7.5, 8.2)).toEqual([7.5, 8.2]);
-});
+describe("Teste de lançamento de notas", () => {
+  test("Nota válida de 1UP e 2UP", () => {
+    const nota1 = 7.5;
+    const nota2 = 8.2;
+    expect(lancarNota(nota1, nota2)).toBe("Notas salvas com sucesso!");
+  });
 
-test("Deve retornar erro para nota 1UP inválida (menor que 0)", () => {
-  expect(lancarNotas(-1.0, 8.2)).toThrow("Nota de 1UP inválida");
-});
+  test("Nota 1UP fora do intervalo", () => {
+    expect(() => lancarNota(-1.0, 8.2)).toThrow(
+      "Nota de 1UP inválida. A nota deve ser um número entre 0.0 e 10.0 com uma casa decimal."
+    );
+  });
 
-test("Deve retornar erro para nota 2UP inválida (maior que 10)", () => {
-  expect(lancarNotas(7.5, 12.0)).toThrow("Nota de 2UP inválida");
-});
+  test("Nota 2UP fora do intervalo", () => {
+    expect(() => lancarNota(7.5, 12.0)).toThrow(
+      "Nota de 2UP inválida. A nota deve ser um número entre 0.0 e 10.0 com uma casa decimal."
+    );
+  });
 
-test("Deve retornar erro para nota 1UP com mais de uma casa decimal", () => {
-  expect(lancarNotas(7.567, 8.2)).toThrow("Nota de 1UP inválida");
-});
+  test("Nota de 1UP com mais de uma casa decimal", () => {
+    expect(() => lancarNota(7.567, 8.2)).toThrowError(
+      "Nota de 1UP inválida. A nota deve ser um número entre 0.0 e 10.0 com uma casa decimal."
+    );
+  });
 
-test("Deve retornar erro para nota 2UP com mais de uma casa decimal", () => {
-  expect(lancarNotas(7.5, 8.256)).toThrow("Nota de 2UP inválida");
-});
+  test("Nota de 2UP com mais de uma casa decimal", () => {
+    expect(() => lancarNota(7.5, 8.256)).toThrowError(
+      "Nota de 2UP inválida. A nota deve ser um número entre 0.0 e 10.0 com uma casa decimal."
+    );
+  });
 
-test("Deve retornar erro para entradas não numéricas", () => {
-  expect(lancarNotas("sete", 8.2)).tothrow("Entradas devem ser numéricas");
+  test("Entradas não numéricas", () => {
+    expect(() => lancarNota("sete", 8.2)).toThrow(
+      "Entradas devem ser numéricas"
+    );
+  });
 });
