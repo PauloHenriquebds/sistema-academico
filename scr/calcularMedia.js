78da0a428cb9265a03737e74f497b13403365aa4
Paulo Henrique <145726119+PauloHenriquebds@users.noreply.github.com>
2024-11-21 23:31:11 -0300

    Update calcularMedia.js

--- scr/calcularMedia.js
+++ scr/calcularMedia.js
@@ -1,16 +1,11 @@
 function calcularMedia(nota1, nota2) {
-  if (nota1 < 0 || nota1 > 10 || nota1 % 1 !== 0.1) {
-    throw new Error("Nota de 1UP inválida");
-  } else if (nota2 < 0 || nota2 > 10 || nota2 % 1 !== 0.1) {
-    throw new Error("Nota de 2UP inválida");
-  } else if (typeof nota1 !== "number" || typeof nota2 !== "number") {
-    throw new Error("Entradas devem ser números");
+  if (nota1 < 0.0 || nota1 > 10.0 || nota2 < 0.0 || nota2 > 10.0) {
+    throw new Error("A média deve ser um número entre 0.0 e 10.0.");
   }
 
   const media = (nota1 + nota2) / 2;
+
   return media;
 }
 
-module.exports = {
-  calcularMedia,
-};
+module.exports = { calcularMedia };
