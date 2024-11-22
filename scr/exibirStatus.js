d760d9a54267b23a6fe8f915b00a88f9e2b08e20
Paulo Henrique <145726119+PauloHenriquebds@users.noreply.github.com>
2024-11-21 23:31:26 -0300

    Update exibirStatus.js

--- scr/exibirStatus.js
+++ scr/exibirStatus.js
@@ -1,13 +1,13 @@
-function definirStatus(media) {
+function obterStatus(media) {
   if (media < 4.0) {
     return "Reprovado por média";
+  } else if (media >= 6.0) {
+    return "Aprovado por média";
   } else if (media >= 4.0 && media < 6.0) {
     return "Aguardando a final";
-  } else {
-    return "Aprovado por média";
   }
 }
 
 module.exports = {
-  definirStatus,
+  obterStatus,
 };
